2ab8ea4c405aebb7fccf2b8bda941bc88adabb16
Matthew Heim <matt@wireless-nat-inside.uiowa.edu>
2018-04-06 12:29:22 -0500

    GymStats now has all of the gyms being loaded from the database

--- app/screens/GymStats.js
+++ app/screens/GymStats.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'; 
-import { View, Text, StyleSheet } from 'react-native'; 
+import {  StyleSheet, ScrollView, View, Alert, Image, Dimensions, TouchableOpacity, TouchableHighlight, Navigator } from 'react-native'; 
 
-import {Container} from '../components/Container'; 
+import {Container} from '../components/Container';
+
+import { Ionicons } from '@expo/vector-icons';
+import AutoHeightImage from 'react-native-auto-height-image';
+import { Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import {firebaseApp} from '../../db/DbConfig';
+
+const deviceWidth = Dimensions.get('window').width;
+
+var pageArray = []
+var nameArray = [];
+var imageArray = [];
+var routeArray = [];
+var weekdayHours = [];
+var weekendHours = [];
 
 class GymStats extends Component{
     static navigationOptions = {
@@ -11,13 +25,104 @@ class GymStats extends Component{
         }, 
           headerTintColor: '#FFFF00',
     };
+
+    constructor(props) {
+        super(props);
+        this.state={
+            gyms: "",
+            images: "test",
+            pages: "",
+            routes: "",
+            WDHours: "",
+            WEHours: "",
+        }
+        var that = this;
+        var mainRef = firebaseApp.database().ref("facilities")
+        
+        mainRef.once("value").then(function(dataSnapshot) {
+          var i = 0;
+          dataSnapshot.forEach(function(testingSnap){
+            nameArray[i] = testingSnap.child("name").val();
+            imageArray[i] = testingSnap.child("image").val();
+            pageArray[i] = testingSnap.child("page").val();
+            weekdayHours[i] = testingSnap.child("hours/open/weekdays").val();
+            weekendHours[i] = testingSnap.child("hours/open/weekends").val();
+            routeArray[i] = i;
+            i++;
+          })
+          that.setState({
+            gyms : nameArray,
+            images : imageArray,
+            pages : pageArray,
+            routes : routeArray,
+            WDHours : weekdayHours,
+            WEHours : weekendHours,
+          })
+        })
+    }
     render(){
+
+        var screen = [];
+        for(i = 0; i < this.state.gyms.length; i++){
+          //console.log(weekdayHours + weekendHours)
+            testpage = "" + this.state.pages[i];
+            console.log(testpage)
+            screen.push(
+                <View style={styles.container}>
+                    <TouchableOpacity activeOpacity={ 0.75 } style={ styles.button } onPress={() => Alert("Hello")}>
+                        <AutoHeightImage width={deviceWidth} source={{uri: this.state.images[i]}} />
+                        <Text style={{textAlign: 'center', fontSize: 30}}>{'\n' + this.state.gyms[i]}</Text>
+                        <Text style={{textAlign: 'center', fontSize: 20, textDecorationLine: 'underline'}}>{'\nHours'}</Text>
+                        <Text style={{textAlign: 'center', fontSize: 15}}>{this.state.WDHours[i]}</Text>
+                        <Text style={{textAlign: 'center', fontSize: 15}}>{this.state.WEHours[i]}</Text>
+                        <Text style={{textAlign: 'center', fontSize: 15}}>{this.state.pages[i]}</Text>
+                    </TouchableOpacity>
+                </View>
+            )
+        }
+    
+
         return (
-                <Container>
-                    <Text> Hello from Gym Stats! </Text>
-                </Container>            
+            <Container>
+	            <Content>
+	                <ScrollView scrollsToTop={true} ref={(ref) => this.myScroll = ref}>
+                        {screen}
+                    </ScrollView>
+	            </Content>
+            </Container>            
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttonContainer: {
+      margin: 20,
+      marginTop: 100
+    },
+    alternativeLayoutButtonContainer: {
+      margin: 20,
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
+    button: {
+        flex: 1,
+        flexDirection: 'column',
+        padding: 0,
+        justifyContent: 'center',
+      marginBottom: 20,
+      shadowColor: '#303838',
+      shadowOffset: { width: 0, height: 5 },
+      shadowRadius: 10,
+      shadowOpacity: 0.35,
+    },
+    title:{
+        
+    }
+  });
 
 export default GymStats; 
