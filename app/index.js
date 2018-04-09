ea9f8ee195669f7eec18347aef134bd344644b02
Matthew Heim <matt@wireless-nat-inside.uiowa.edu>
2018-04-09 11:52:42 -0500

    Couple changes

--- app/index.js
+++ app/index.js
@@ -5,19 +5,11 @@ import EStyleSheet from "react-native-extended-stylesheet";
 import HerkNav from "./screens/HerkNav"; 
 
 EStyleSheet.build({
-<<<<<<< HEAD
     $white: '#FFFFFF',
     $black: '#000000',
     $gold: '#facf33',
     $grey: '#4C4C4C', 
     $red: '#ff0000',
-=======
-    $white: "#FFFFFF",
-    $black: "#000000",
-    $gold: "#FFFF00",
-    $grey: "#4C4C4C", 
-    $red: "#ff0000",
->>>>>>> 415384f79fd7cadc19fdfec7f49fd31b598e83ea
 
 });
 //export default () => <Home/>;
