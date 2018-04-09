dd57fbd6644e01e296005839a00752f70685428d
dichha <dichha.chamling@gmail.com>
2018-04-09 12:19:58 -0500

    resolved merge conflict

--- app/index.js
+++ app/index.js
@@ -5,7 +5,6 @@ import EStyleSheet from "react-native-extended-stylesheet";
 import HerkNav from "./screens/HerkNav"; 
 
 EStyleSheet.build({
-<<<<<<< HEAD
     $white: '#FFFFFF',
     $black: '#000000',
     $gold: '#FFFF00',
@@ -14,13 +13,6 @@ EStyleSheet.build({
     $red: '#ff0000',
     $textInput: '#262626',
     $success: '#006600',  
-=======
-    $white: "#FFFFFF",
-    $black: "#000000",
-    $gold: "#FFFF00",
-    $grey: "#4C4C4C", 
-    $red: "#ff0000",
->>>>>>> 415384f79fd7cadc19fdfec7f49fd31b598e83ea
 
 });
 //export default () => <Home/>;
