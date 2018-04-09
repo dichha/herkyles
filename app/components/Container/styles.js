b4b5016247972b2482344fbda168e0277baefd60
Matthew Heim <matt@wireless-nat-inside.uiowa.edu>
2018-04-09 11:51:21 -0500

    Couple changes

--- app/components/Container/styles.js
+++ app/components/Container/styles.js
@@ -3,14 +3,8 @@ import EStyleSheet from "react-native-extended-stylesheet";
 export default EStyleSheet.create({
   container: {
     flex: 1,
-<<<<<<< HEAD
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '$white',
-=======
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "$gold",
->>>>>>> 415384f79fd7cadc19fdfec7f49fd31b598e83ea
   },
 });
