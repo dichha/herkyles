bfcffc4ee72ee2174414322fcaf24fbf4fa5c4e7
Nathan Smith <nathan-d-smith@uiowa.edu>
2018-05-01 08:44:32 -0500

    camera is working

--- app/screens/QrScanner.js
+++ app/screens/QrScanner.js
@@ -1,53 +1,56 @@
-import React, { Component } from "react"; 
-import { View, Text, AppRegistry, StyleSheet, TouchableOpacity, Linking, StatusBar} from "react-native"; 
-import { StackNavigator } from "react-navigation"; 
- 
-import Container from "../components/Container/Container";
-//import RNCamera from 'react-native-camera';
-//import QRCodeScanner from 'react-native-qrcode-scanner';
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
 
-class QrScanner extends Component {
-    static navigationOptions = {
-        title: "QR Scanner", 
-        headerStyle: {
-            backgroundColor: "#000000",
-        }, 
-          headerTintColor: '#facf33',
-    };
+export default class CameraExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
 
-    render(){
-        return (
-            <Container>
-                <Text style={{textAlign: 'center', fontSize: 15}}>QR Scanner</Text>
-            </Container>
-        );
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
     }
-
-//     onSuccess(e) {
-//         Linking
-//         .openURL(e.data)
-//         .catch(err => console.error('An error occurred', err));
-//     }
-
-//     render(){
-//         return (
-//             <QRCodeScanner
-//                 onRead={this.onSuccess.bind(this)}
-//                 topContent={
-//                     <Text style={{textAlign: 'center', fontSize: 15}}>
-//                         Scan the QR code.
-//                     </Text>
-//                 }
-//                 bottomContent={
-//                     <Text style={{textAlign: 'center', fontSize: 15}}>
-//                         OK. Got it!
-//                     </Text>
-//                 }
-//             />
-//         );
-//     }
+  }
 }
-
-export default QrScanner;
-
-// AppRegistry.registerComponent('default', () => QrScanner);
