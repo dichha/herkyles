f78e44de73e54c0c6996bb27884c283f5a74ce43
Tony Maiers <tmaiers13@gmail.com>
2018-05-02 11:30:01 -0500

    added group fitness schedule buton on home page but can't get db reference to work so hard coded link for now

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react"; 
-import { View, Text, StyleSheet, StatusBar} from "react-native"; 
+import { View, Text, StyleSheet, StatusBar, Linking} from "react-native"; 
 import { StackNavigator } from "react-navigation"; 
  
 import Container from "../components/Container/Container";
 import LinkBtns from "../components/Buttons/LinkBtns/LinkBtns"; 
 import Logo from "../components/Logo/Logo";
+import {firebaseApp} from '../../db/DbConfig';
  
 
 class Home extends Component{
@@ -15,16 +16,42 @@ class Home extends Component{
         }, 
           headerTintColor: '#facf33',
     };
+
+
     handleGymStatsPress = () => {
         this.props.navigation.navigate("GymStats"); 
         console.log("handle gym stats press"); 
+    }
+
+    handleGroupPress = () => {
+        Linking.openURL(this.state.link);
+        console.log("handle group press"); 
     }
 
     handleLoginPress = () => {
         this.props.navigation.navigate("Login"); 
         console.log("handle login Press"); 
     }
+
+    constructor(props){
+        super(props);
+
+        this.state={
+            link: "",
+        }
+        var that = this;
+        var mainRef = firebaseApp.database().ref("facilities/groupFitnessScheduleLink")
+        var buttonLink = "";
+        mainRef.once("value").then(function(dataSnapshot) {
+            gymDB=dataSnapshot;
+            buttonLink = dataSnapshot.val();
+            })
+            that.setState({
+                link: buttonLink,
+            })
+    }
     render(){
+        console.log("this.state.link="+this.state.link);
         return (
             <Container>
                 
@@ -33,6 +60,10 @@ class Home extends Component{
                 <LinkBtns
                     text="Gyms"
                     onPress={this.handleGymStatsPress}
+                />
+                <LinkBtns
+                    text="Group Fitness Schedule"
+                    onPress={()=> {Linking.openURL("https://recserv.uiowa.edu/programs/fitness/group-fitness/group-fitness-schedule")}}
                 />
                 <LinkBtns
                     text="Admin Login"
