ba98b9712dba9c73560c28d411556f2d637625d5
Nathan Smith <nathan-d-smith@uiowa.edu>
2018-05-01 21:38:17 -0500

    added timeout for tests

--- __tests__/screens/App.test.js
+++ __tests__/screens/App.test.js
@@ -3,6 +3,8 @@ import App from "../../App";
 
 import renderer from "react-test-renderer";
 
+afterAll(() => setTimeout(() => process.exit(), 100000));
+
 describe ("App", () => {
   it("renders without crashing", () => {
     const rendered = renderer.create(<App />).toJSON();
